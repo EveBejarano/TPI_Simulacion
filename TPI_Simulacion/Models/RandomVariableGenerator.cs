0738e47c429db8d0e0ad8c41791de81ce6b3c675
Evelyn Bejarano <evebejarano@hotmail.com>
2018-06-21 17:22:52 -0300

    Add project files.

---
+++ TPI_Simulacion/Models/RandomVariableGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TPI_Simulacion.Models
+{
+    class RandomVariableGenerator
+    {
+
+        public int DecimalAmount { get; set; }
+
+        public double fdp { get; set; }
+
+        public float GenerateRandomVariable()
+        {
+            Random RandomNumberGenerator = new Random();
+            var r = RandomNumberGenerator.Next(0, DecimalAmount);
+            r = r / DecimalAmount;
+            return r;
+        }
+
+        public double GenerateRandomNumber(float r)
+        {
+            return (fdp * r);
+        }
+
+    }
+}
---
+++ TPI_Simulacion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TPI_Simulacion.Models;
+
+namespace TPI_Simulacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomVariableGenerator Generator = new RandomVariableGenerator
+            {
+                DecimalAmount = 10000,
+                fdp = 0.002083
+            };
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
